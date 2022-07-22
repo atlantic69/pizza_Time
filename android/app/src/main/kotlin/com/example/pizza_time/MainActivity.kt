739d5a569bc9abe4c5bf197f6c5f1b73a9c7cefb
Atlantic <polosuhin.99@gmail.com>
2022-07-23 05:10:37 +0700

    Created a menu's page

---
+++ android/app/src/main/kotlin/com/example/pizza_time/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.pizza_time
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+import 'package:pizza_time/pages/menu.dart';
+
+void main() => runApp(const MaterialApp(
+  home: Menu(),
+));
+
+//https://cdn.dodostatic.net/static/Img/Products/57157f013c164840a24c1d49c7adb3b6_292x292.jpeg
